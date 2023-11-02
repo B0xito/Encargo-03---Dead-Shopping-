a6c25961c7aafeb69130e334d477180601639aba
B0xito <zarystabof@gmail.com>
2023-11-02 00:34:45 -0300

    v0.1.1
    - Jump

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -6,7 +6,9 @@ public class PlayerInteractions : MonoBehaviour
 {
 
     [SerializeField] Rigidbody playerRb;
-    [SerializeField] float playerSpeed = 5;
+    [SerializeField] private float playerSpeed = 5;
+    [SerializeField] private float playerJumpHeight = 5;
+    [SerializeField] private bool isGrounded;
 
     void Start()
     {
@@ -20,8 +22,18 @@ public class PlayerInteractions : MonoBehaviour
 
     void Movement()
     {
+        #region Movement
         float h = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
         float v = Input.GetAxis("Vertical") * playerSpeed + Time.deltaTime;
         transform.Translate(h, 0 , v);
+        #endregion
+
+        #region Jump
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.
+        }
+        #endregion
     }
 }
